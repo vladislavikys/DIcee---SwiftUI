d1293e578ac81841401c09a0bf5cdbaa9ed26246
vladislavikys <zarya.vlad@yandex.ru>
2022-09-05 15:33:31 +0300

    add back-dice-button

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack{
+                    DiceeView(n: 1)
+                    DiceeView(n: 1)
+                }
+                .padding()
+                Spacer()
+                Button("Roll") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 60))
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.red)
+                .cornerRadius(33)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceeView: View {
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
     }
 }
