ed88420565f077c714088d8ac7f7d5ca37a8048c
vladislavikys <zarya.vlad@yandex.ru>
2022-09-05 15:18:49 +0300

    init

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by vlad zarya on 5.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
